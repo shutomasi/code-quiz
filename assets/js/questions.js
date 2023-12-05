e3410c7fa7b8e81460851fdcdcbc1d8d488869d0
shutomasi <samic4702@gmail.com>
2023-12-05 15:53:26 +0000

    Removed unnecessary files and reorganised file structure

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+// questions array
+const quizData = [
+    {
+      // question object
+      question: "What does HTML stand for?",
+      answers: ["Hyper Text Markup Language", "Highly Typed Machine Learning", "Hyper Transfer Markdown Language", "High-level Text Management Language"],
+      correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+      question: "Which tag is used to link an external CSS file in HTML?",
+      answers: ["<link>", "<style>", "<css>", "<script>"],
+      correctAnswer: "<link>",
+    },
+    {
+      question: "What does CSS stand for?",
+      answers: ["Computer Style Sheets", "Creative Style Selector", "Cascading Style Sheets", "Colorful Styling System"],
+      correctAnswer: "Cascading Style Sheets",
+    },
+    {
+      question: "What is the purpose of the JavaScript 'addEventListener' method?",
+      answers: ["To add a new event handler to an HTML element", "To create a new JavaScript function", "To include an external JavaScript file", "To modify the document's structure"],
+      correctAnswer: "To add a new event handler to an HTML element",
+    },
+    {
+      question: "What is the purpose of the 'typeof' operator in JavaScript?",
+      answers: ["To check the data type of a variable", "To declare a new variable", "To create a function", "To perform mathematical operations"],
+      correctAnswer: "To check the data type of a variable",
+    },
+    {
+      question: "What is the purpose of the HTML 'meta' tag?",
+      answers: ["To define metadata about an HTML document", "To create a navigation link", "To include external scripts", "To specify a page title"],
+      correctAnswer: "To define metadata about an HTML document",
+    },
+    {
+      question: "In CSS, what property is used to set the background color of an element?",
+      answers: ["background-color", "color", "background", "bgcolor"],
+      correctAnswer: "background-color",
+    },
+    {
+      question: "Which HTML element is used to define the structure of an HTML document?",
+      answers: ["<html>", "<head>", "<body>", "<doctype>"],
+      correctAnswer: "<html>",
+    },
+    {
+      question: "What does the acronym API stand for in the context of web development?",
+      answers: ["Application Programming Interface", "Advanced Programming Interface", "Automated Programming Interface", "Application Protocol Interface"],
+      correctAnswer: "Application Programming Interface",
+    },
+    {
+      question: "How do you access the first element of an array in JavaScript?",
+      answers: ["array[0]", "array[1]", "array.first", "array.firstElement"],
+      correctAnswer: "array[0]",
+    },
+  ];
